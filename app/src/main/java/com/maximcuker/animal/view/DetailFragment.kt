7c8367b7db331d2c0edb80445cc91062bb390bd0
maximilian6547s <maximilian.s517@gmail.com>
2021-06-16 11:26:13 +0300

    Extract color from image in DetailFragment.kt using Palette library and set it as background color of layout

--- app/src/main/java/com/maximcuker/animal/view/DetailFragment.kt
+++ app/src/main/java/com/maximcuker/animal/view/DetailFragment.kt
@@ -1,11 +1,17 @@
 package com.maximcuker.animal.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.maximcuker.animal.R
 import com.maximcuker.animal.model.Animal
 import com.maximcuker.animal.util.getProgressDrawable
@@ -36,5 +42,28 @@ class DetailFragment : Fragment() {
         animalLocation.text = animal?.location
         animalLifespan.text = animal?.lifeSpan
         animalDiet.text = animal?.diet
+
+        animal?.imageUrl?.let {
+            setupBackgroundColor(it)
+        }
+    }
+
+    private fun setupBackgroundColor(imageUrl: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(imageUrl)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate() { palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+                            animalDetailLayout.setBackgroundColor(intColor)
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+            })
     }
 }
