997ae15ef51227dc8dcd6a97a6d7b799e8776f75
maximilian6547s <maximilian.s517@gmail.com>
2021-06-17 14:26:48 +0300

    Testing animals API success result

--- app/src/test/java/com/maximcuker/animal/ListViewModelTest.kt
+++ app/src/test/java/com/maximcuker/animal/ListViewModelTest.kt
@@ -5,15 +5,19 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.maximcuker.animal.di.ApiModule
 import com.maximcuker.animal.di.AppModule
 import com.maximcuker.animal.di.DaggerViewModelComponent
+import com.maximcuker.animal.model.Animal
 import com.maximcuker.animal.model.AnimalApiService
 import com.maximcuker.animal.util.SharedPreferencesHelper
 import com.maximcuker.animal.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
@@ -33,6 +37,8 @@ class ListViewModelTest {
 
     var listViewModel = ListViewModel(application,true)
 
+    private val key = "Test key"
+
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
@@ -43,6 +49,23 @@ class ListViewModelTest {
             .prefsModule(PrefsModuleTest(prefs))
             .build()
             .inject(listViewModel)
+    }
+
+    @Test
+    fun getAnimalsSuccess() {
+        Mockito.`when`(prefs.getApiKey()).thenReturn(key)
+        val animal = Animal("cow", null, null, null,null,null,null)
+        val animalList = listOf<Animal>(animal)
+        val testSingle = Single.just(animalList)
+
+        Mockito.`when`(animalService.getAnimals(key)).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.animals.value?.size)
+        Assert.assertEquals(false, listViewModel.loadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+
     }
 
     @Before
